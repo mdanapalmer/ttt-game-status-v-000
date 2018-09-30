69f75c3130ac5b07226b731aeacf051edd657f23
Dana Palmer <roiai4@hotmail.com>
2018-09-30 23:17:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2], 
+  [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
   [0, 3, 6],
@@ -15,37 +15,28 @@ WIN_COMBINATIONS = [
   [2, 4, 6],
   [0, 4, 8]
   ]
-  
+
   def won?(board)
-    WIN_COMBINATIONS.each do |win_move|
-      if board[win_move[0]] == "X" && board[win_move[1]] == "X" && board[win_move[2]]== "X"
-        return win_move
-      elsif board[win_move[0]] == "O" && board[win_move[1]] == "O" && board[win_move[2]] == "O"
-        return win_move
-      end
-   end
-   return false 
- end
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+  end
+end
+
 
   def full?(board)
      board.all?{|token| token == "X" || token == "O"}
   end
-  
+
   def draw?(board)
     full?(board) && !won?(board)
   end
-  
+
   def over?(board)
     full?(board) || won?(board)
   end
-  
+
   def winner(board)
     if winning_move = won?(board)
       board[winning_move.first]
     end
   end
-  
-  
-  
-  
-  
